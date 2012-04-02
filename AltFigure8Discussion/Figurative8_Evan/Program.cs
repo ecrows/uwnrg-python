3e14fa71e6e42fda7c5803b25256c0cb0a3a4294
unknown <Evan Crothers@Icarus.(none)>
2012-04-02 17:13:16 -0400

    Just thought I'd upload the discussion I wrote a while ago re: the alternative Figure 8 implementation.

---
+++ AltFigure8Discussion/Figurative8_Evan/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Figurative8_Evan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Figure8Test();
+
+        }
+    }
+}
