a81fa243613f0c19b203e19428577e84eeb08068
Matt Maclean <matthewcmaclean@gmail.com>
2013-01-12 19:22:59 -0500

    Austin's work. Webcam interface setup; however, no pictures or video support. Video is not placed in the main window yet either.

--- UWNRG_CURRENT/main.py
+++ UWNRG_CURRENT/main.py
@@ -62,6 +62,39 @@ class  MainWindow:
                                   self.__x_axis_inverted,
                                   self.__y_axis_inverted)
 
+#gstreamer setup
+    def start_stop(self, w):
+        if self.button.get_label() == "Start":
+            self.button.set_label("Stop")
+            self.player.set_state(gst.STATE_PLAYING)
+        else:
+            self.player.set_state(gst.STATE_NULL)
+            self.button.set_label("Start")
+
+    def exit(self, widget, data=None):
+        gtk.main_quit()
+
+    def on_message(self, bus, message):
+            t = message.type
+            if t == gst.MESSAGE_EOS:
+                    self.player.set_state(gst.STATE_NULL)
+                    #self.button.set_label("Start")
+            elif t == gst.MESSAGE_ERROR:
+                    err, debug = message.parse_error()
+                    print "Error: %s" % err, debug
+                    self.player.set_state(gst.STATE_NULL)
+                    #self.button.set_label("Start")
+
+    def on_sync_message(self, bus, message):
+            if message.structure is None:
+                    return
+            message_name = message.structure.get_name()
+            if message_name == "prepare-xwindow-id":
+                    # Assign the viewport
+                    imagesink = message.src
+                    imagesink.set_property("force-aspect-ratio", True)
+                    imagesink.set_xwindow_id(self.movie_window.window.xid)
+#setup end
     def __init__(self):
         filename = "GUI.glade"
         handlers = {
@@ -93,6 +126,40 @@ class  MainWindow:
 
         self.__log.set_buffer(self.__builder.get_object(
                 "vertical_log_scroll_window_text_view").get_property('buffer'))
+#gstreamer start
+        """window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_title("Webcam-Viewer")
+        window.set_default_size(500, 400)
+        window.connect("destroy", gtk.main_quit, "WM destroy")
+        vbox = gtk.VBox()
+        window.add(vbox)"""
+        self.movie_window = self.__builder.get_object("camera_feed_drawing_area")
+        imagesink = gst.element_factory_make("ximagesink", "sink")
+        imagesink.set_property("force-aspect-ratio", True)
+        
+        imagesink.set_xwindow_id(self.movie_window.window.xid)
+        """vbox.add(self.movie_window)
+        hbox = gtk.HBox()
+        vbox.pack_start(hbox, False)
+        hbox.set_border_width(10)
+        hbox.pack_start(gtk.Label())
+        self.button = gtk.Button("Start")
+        self.button.connect("clicked", self.start_stop)
+        hbox.pack_start(self.button, False)
+        self.button2 = gtk.Button("Quit")
+        self.button2.connect("clicked", self.exit)
+        hbox.pack_start(self.button2, False)
+        hbox.add(gtk.Label())
+        window.show_all()"""
+
+        # Set up the gstreamer pipeline
+        self.player = gst.parse_launch ("ksvideosrc device-index=0 ! ffmpegcolorspace ! autovideosink")
+        bus = self.player.get_bus()
+        bus.add_signal_watch()
+        bus.enable_sync_message_emission()
+        bus.connect("message", self.on_message)
+        self.player.set_state(gst.STATE_PLAYING)
+    #gstreamer end
 
     def __invert_x_axis(self, check_menu_item):
         """ Updates x inversion variable
