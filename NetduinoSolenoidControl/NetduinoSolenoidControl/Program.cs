8dbaf446392ea953b1b37a88a2207ab1f6514524
Evan Crothers <evan.crothers@gmail.com>
2013-04-21 22:55:19 -0400

    Added C# Netduino Plus 2 code with PWM support.  Also includes .gitignore for Visual Studio.

---
+++ NetduinoSolenoidControl/NetduinoSolenoidControl/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.IO;
+using System.Text;
+using System.IO.Ports;
+
+namespace NetduinoSolenoidControl
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var interf = Microsoft.SPOT.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()[0];
+            interf.EnableStaticIP("10.0.0.32", "255.255.255.0", "0.0.0.0");
+            WebServer webServer = new WebServer();
+            webServer.ListenForRequest();
+        }
+    }
+}
