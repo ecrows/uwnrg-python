6d294a73f57d3c4be3d61ff30c8fb2b1cd40b8f6
Evan Crothers <evan.crothers@gmail.com>
2013-09-12 20:24:41 -0400

    Forgot to push updated image_test.py.

--- UWNRG_CURRENT/image_test.py
+++ UWNRG_CURRENT/image_test.py
@@ -1,22 +1,16 @@
 import imaging.image_recognition as imaging
 import cv2 as cv2  
-import numpy as np
-
-#vc = cv2.VideoCapture("MobilityRun1.wmv")
-vc = cv2.VideoCapture("MicroassemblyRun1.wmv")
-#vc = cv2.VideoCapture(1)
-rval, frame = vc.read()
 
 irtest = imaging.ImageRecognition("micro")
 testfield = irtest.new_field()
 
 cv2.namedWindow("preview")
-testfield.find_field(frame)
-processed = testfield.show_robot(frame)
-while (rval): 
-    cv2.imshow("preview", processed)  
-    processed = testfield.show_robot(frame)
-    rval, frame = vc.read()
+processed = testfield.show_robot(irtest.get_frame())
+
+while (True):
+    cv2.imshow("preview", processed) 
+
+    processed = testfield.show_robot(irtest.get_frame())
 
     #Should show console output of simplified array data
     #Ideally eventually in the form of the path to be taken
@@ -26,4 +20,5 @@ while (rval):
     key = cv2.waitKey(1)
     if key == 27: # exit on ESC
         break
+
 #raw_input("Press key to continue")
